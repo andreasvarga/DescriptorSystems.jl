832e4b7565d8eedeb14db68b60e39b8a9151ce2d
DSVarga <varga.andreas@gmail.com>
2021-04-24 16:58:50 +0200

    Importing explicitly poles from Polynomials 2.0.9

--- src/DescriptorSystems.jl
+++ src/DescriptorSystems.jl
@@ -12,7 +12,7 @@ import LinearAlgebra: BlasFloat, BlasReal, BlasComplex, copy_oftype, transpose, 
 import Base: +, -, *, /, \, (==), (!=), ^, isapprox, iszero, convert, promote_op, size, length, ndims, 
              hcat, vcat, hvcat, inv, show, lastindex, require_one_based_indexing, print, show, one, zero, eltype
 import MatrixPencils: isregular, rmeval
-import Polynomials: variable, degree
+import Polynomials: variable, degree, poles
 
 export DescriptorStateSpace, AbstractDescriptorStateSpace, dss, dssdata, rdss, rss, iszero, order
 export AbstractRationalFunction, RationalTransferFunction, rtf
