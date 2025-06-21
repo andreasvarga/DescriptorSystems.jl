a831efbe868f12d849d071636d01f3f3117b1185
DSVarga <varga.andreas@gmail.com>
2025-06-21 17:20:15 +0200

    Finish tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,26 +4,26 @@ using Test, DescriptorSystems
 
 @testset "Test DescriptorSystems" begin
 # test constructors
-# include("test_dss.jl")
-# include("test_rtf.jl")
-# include("test_polrat.jl")
-# include("test_connections.jl")
-# # test basic functions
-# include("test_conversions.jl")
-# include("test_timeresp.jl")
-# include("test_ordred.jl")
+include("test_dss.jl")
+include("test_rtf.jl")
+include("test_polrat.jl")
+include("test_connections.jl")
+# test basic functions
+include("test_conversions.jl")
+include("test_timeresp.jl")
+include("test_ordred.jl")
 include("test_analysis.jl")
 # test factorizations
-# include("test_pscf.jl")
-# include("test_cfid.jl")
-# include("test_iofac.jl")
-# # test advanced operations
-# include("test_gsdec.jl")
-# include("test_nullrange.jl")
-# include("test_covers.jl")
-# include("test_linsol.jl")
-# include("test_ginv.jl")
-# include("test_model_matching.jl")
+include("test_pscf.jl")
+include("test_cfid.jl")
+include("test_iofac.jl")
+# test advanced operations
+include("test_gsdec.jl")
+include("test_nullrange.jl")
+include("test_covers.jl")
+include("test_linsol.jl")
+include("test_ginv.jl")
+include("test_model_matching.jl")
 end
 
 end
